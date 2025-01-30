b079e49e4bf4600324abc8c63edb2f0f54d4f273
Darren <nardol.darren@gmail.com>
2025-01-30 20:43:23 +0100

    create a button that generate a random number

---
+++ src/components/dice.jsx
@@ -0,0 +1,21 @@
+import { useState } from "react";
+
+function Dice() {
+  const [dice, setDice] = useState(1);
+
+  const rollDice = () => {
+    const randomNumber = () => Math.floor(Math.random() * 6) + 1;
+    //Math.floor arrondi vers le chiffre du bas
+    //Math.random donne un chiffre aléatoire entre 0 et 0.9999999
+    setDice(randomNumber);
+  };
+
+  return (
+    <>
+      <div className="dice">{dice}</div>
+      <button onClick={rollDice}>Lancer le dé</button>
+    </>
+  );
+}
+
+export default Dice;
